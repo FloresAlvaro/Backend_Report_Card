2f36724d654c6ea35bcb0d120f7458844b1ee1ca
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2025-08-31 23:56:04 -0400

    refactor: improve e2e test structure and add type assertion for request

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Express } from 'express';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,11 +16,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer() as Express)
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(200);
+
+    expect(response.text).toBe('Hello World!');
   });
 });
