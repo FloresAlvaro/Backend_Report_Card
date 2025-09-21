f6d616c60787dfbf0708a6823053879193791722
Alvaro Flores <alvarofloresfarfan2802@gmail.com>
2025-09-21 11:20:40 -0400

    feat: add Seed module with seeding functionality and integrate into main application (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { GradesModule } from './grades/grades.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { StudentSubjectsModule } from './student-subjects/student-subjects.module';
+import { SeedModule } from './seeds/seed.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { StudentSubjectsModule } from './student-subjects/student-subjects.modul
     GradesModule,
     SubjectsModule,
     StudentSubjectsModule,
+    SeedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
